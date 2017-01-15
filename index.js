38fad76471e4cdd73285d7b232ca06e732d4fe5d
Jeevan James <jeevanjj@live.com>
2017-01-15 16:28:09 -0500

    Initial code commit. Copied old ng1-http generator for Angular 1.

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const modeMappings = {
+    'ng1-typescript': 'ng1-typescript',
+    'ng1-ts': 'ng1-typescript'
+};
+
+
+function generate(definition, profile) {
+    let mode = modeMappings[profile.mode || 'ng1-typescript'];
+    let Generator = require(`./lib/${mode}`);
+    let generator = new Generator(definition, profile);
+    return generator.generate();
+}
+
+function validateProfile(profile) {
+    let options = profile.options;
+    if (!options) {
+        throw `Specify an 'options' section in your profile.`;
+    }
+    if (!options.namespaces) {
+        throw `Specify an 'options.namespaces' section in your profile.`;
+    }
+    if (!options.namespaces.services || !options.namespaces.models) {
+        throw `Specify namespaces for services and models under the 'options.namespaces' section using keys 'services' and 'models'.`;
+    }
+}
+
+module.exports = {
+    modes: require('./lib/metadata'),
+    generate: generate,
+    validateProfile: validateProfile
+};
