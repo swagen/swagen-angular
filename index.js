1cd0ab72e246c95c269827bc3b92498fae0722de
Jeevan James <jeevanjj@live.com>
2017-06-17 02:07:59 +0530

    Adding Angular generator

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const _ = require('lodash');
+
 const modeMappings = {
     'ng1-typescript': 'ng1-typescript',
     'ng1-ts': 'ng1-typescript',
-    'ng1-typescript-experimental': 'ng1-typescript-experimental'
+    'ng1-typescript-experimental': 'ng1-typescript-experimental',
+    'ng-typescript': 'ng-typescript',
+    'ng-ts': 'ng-typescript',
 };
 
 
@@ -16,14 +20,16 @@ function generate(definition, profile) {
 
 function validateProfile(profile) {
     let options = profile.options;
-    if (!options) {
-        throw `Specify an 'options' section in your profile.`;
-    }
-    if (!options.namespaces) {
-        throw `Specify an 'options.namespaces' section in your profile.`;
-    }
-    if (!options.namespaces.services || !options.namespaces.models) {
-        throw `Specify namespaces for services and models under the 'options.namespaces' section using keys 'services' and 'models'.`;
+    if (_.startsWith(profile.mode, 'ng1-')) {
+        if (!options) {
+            throw `Specify an 'options' section in your profile.`;
+        }
+        if (!options.namespaces) {
+            throw `Specify an 'options.namespaces' section in your profile.`;
+        }
+        if (!options.namespaces.services || !options.namespaces.models) {
+            throw `Specify namespaces for services and models under the 'options.namespaces' section using keys 'services' and 'models'.`;
+        }
     }
 }
 
