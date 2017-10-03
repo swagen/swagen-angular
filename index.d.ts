3642e4cc9dc3b87dc8bc26b75e5792430f80c7be
Jeevan James <jeevanjj@live.com>
2017-10-03 08:45:58 +0530

    Updated Typescript definition file and validators.

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,88 @@
-declare interface Options {
+/**
+ * Options for Angular 2+ generators.
+ */
+declare interface AngularTypescriptOptions {
+    generation?: {
+        generateInterfaces?: boolean;
+        generateImplementations?: boolean;
+    };
+
+    baseUrl?: {
+        strategy?: 'ImportedVar'|'Property'|'InjectedToken'|'Swagger';
+        overrideUrl?: string;
+        importVar?: {
+            importFrom: string;
+            importVariable: string;
+            property?: string;
+        };
+        property?: string;
+    };
+
+    angular?: {
+        httpFramework?: 'HttpClient'|'Http';
+        tokenType?: 'InjectedToken'|'OpaqueToken';
+        futuresType?: 'Observables'|'Promises';
+    };
+
+    customizations?: {
+        importFrom: string;
+        importVariable: string;
+    };
+}
+
+/**
+ * Options for AngularJS 1.x generators.
+ */
+declare interface AngularJsTypescriptOptions {
     /**
-     * Name of the opaque token used for injecting the base URL value.
-     * Defaults to 'API_BASE_URL'
+     * Name of the Angular module to register the generated services under.
      */
-    baseUrlToken?: string | undefined;
+    moduleName?: string;
 
     /**
-     * Suffix to add to each service class name.
-     * Defaults to 'Client'
+     * Suffix to add to each service name.
+     * Defaults to 'Client' if not specified.
      */
-    serviceClassSuffix?: string | undefined;
+    serviceSuffix: string;
+
+    /**
+     * Details on how to access the base URL of web service.
+     */
+    baseUrl: {
+        /**
+         * The Typescript type of the service containing the base URL.
+         */
+        type: string;
+
+        /**
+         * The injectable name of the Angular service containing the base URL.
+         */
+        provider: string;
+
+        /**
+         * Array of member names needed to access the base URL in the service.
+         */
+        path?: string[];
+    };
+
+    /**
+     * Typescript namespaces to generate the services and models in.
+     */
+    namespaces: {
+        /**
+         * Typescript namespace to generate the services interfaces and classes.
+         */
+        services: string;
+
+        /**
+         * Typescript namespace to generate the models and model factories.
+         */
+        models: string;
+    };
+
+    /**
+     * Reference paths to be added to the top of the generated code.
+     * Only specify the path; do not specify the full markup.
+     */
+    references?: string[];
 }
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const _ = require('lodash');
-
 const modeMappings = {
     'ng1-typescript': 'ng1-typescript',
     'ng1-ts': 'ng1-typescript',
@@ -10,31 +8,23 @@ const modeMappings = {
     'ng-ts': 'ng-typescript',
 };
 
-
 function generate(definition, profile) {
-    let mode = modeMappings[profile.mode || 'ng1-typescript'];
-    let Generator = require(`./lib/${mode}`);
-    let generator = new Generator(definition, profile);
+    const mode = modeMappings[profile.mode || 'ng-typescript'];
+    const Generator = require(`./lib/${mode}`);
+    const generator = new Generator(definition, profile);
     return generator.generate();
 }
 
 function validateProfile(profile) {
-    let options = profile.options;
-    if (_.startsWith(profile.mode, 'ng1-')) {
-        if (!options) {
-            throw `Specify an 'options' section in your profile.`;
-        }
-        if (!options.namespaces) {
-            throw `Specify an 'options.namespaces' section in your profile.`;
-        }
-        if (!options.namespaces.services || !options.namespaces.models) {
-            throw `Specify namespaces for services and models under the 'options.namespaces' section using keys 'services' and 'models'.`;
-        }
+    const mode = modeMappings[profile.mode || 'ng-typescript'];
+    const validator = require(`./lib/${mode}/validator`);
+    if (typeof validator === 'function') {
+        validator(profile.options);
     }
 }
 
 module.exports = {
     modes: require('./lib/metadata'),
-    generate: generate,
-    validateProfile: validateProfile
+    generate,
+    validateProfile
 };
