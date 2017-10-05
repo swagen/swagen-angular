00ef8414eac739da72b3d0ba731c89e866596658
Jeevan James <jeevanjj@live.com>
2017-10-06 02:15:10 +0530

    Updated the package to conform to the new contract.

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 'use strict';
 
-const modeMappings = {
-    'ng-typescript': 'ng-typescript',
-    'ng-ts': 'ng-typescript',
-    'ng1-typescript': 'ng1-typescript',
-    'ng1-ts': 'ng1-typescript'
-};
-
-function generate(definition, profile) {
-    const mode = modeMappings[profile.mode || 'ng-typescript'];
-    const Generator = require(`./lib/${mode}`);
-    const generator = new Generator(definition, profile);
-    return generator.generate();
-}
-
-function validateProfile(profile) {
-    const mode = modeMappings[profile.mode || 'ng-typescript'];
-    try {
-        const validator = require(`./lib/${mode}/validator`);
-        if (typeof validator === 'function') {
-            validator(profile.options);
-        }
-    } catch(ex) {
-        // Do nothing
-    }
-}
-
-module.exports = {
-    modes: require('./lib/metadata'),
-    generate,
-    validateProfile
-};
+module.exports = [
+    require('./lib/ng-typescript'),
+    require('./lib/ng1-typescript')
+];
